07249b3a8677c7a8e62ed7f1070adbf761c23b9f
Davilson Paulino da Cunha Junior <davilson.junior@stitcloud.com>
2023-08-20 20:01:24 -0300

    Add lambda admin and api gateway

--- lib/ecommerceApi-stack.ts
+++ lib/ecommerceApi-stack.ts
@@ -7,6 +7,7 @@ import { Construct } from "constructs";
 
 interface ECommerceApiStackProps extends cdk.StackProps {
   productsFetchHandler: lambdaNodeJS.NodejsFunction;
+  productsAdminHandler: lambdaNodeJS.NodejsFunction
 }
 
 export class ECommerceApiStack extends cdk.Stack {
@@ -38,7 +39,22 @@ export class ECommerceApiStack extends cdk.Stack {
     );
 
     // "/products" --- "/" = root
-    const productsResources = api.root.addResource("products");
-    productsResources.addMethod("GET", productsFetchIntegration);
+    const productsResource = api.root.addResource("products");
+    productsResource.addMethod("GET", productsFetchIntegration);
+
+   // GET /products/{id}
+    const productIdResource = productsResource.addResource("{id}")
+    productIdResource.addMethod("GET", productsFetchIntegration)
+
+    const productsAdminIntegration = new apigateway.LambdaIntegration(props.productsAdminHandler)
+
+   // POST /products
+    productsResource.addMethod("POST", productsAdminIntegration)
+
+    // PUT /products/{id}
+    productIdResource.addMethod("PUT", productsAdminIntegration)
+
+    // DELETE /products/{id}
+    productIdResource.addMethod("DELETE", productsAdminIntegration)
   }
 }
