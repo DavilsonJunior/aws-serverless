dfa24100f58224d39dda6ad59123469cbaca7035
Davilson Paulino da Cunha Junior <davilson.junior@stitcloud.com>
2023-11-18 11:14:50 -0300

    Criando o código da função de geração de eventos de alteração de produtos

---
+++ lambda/products/layers/productEventsFunction.ts
@@ -0,0 +1,47 @@
+import { ProductEvent } from "/opt/nodejs/productEventsLayer";
+import { Callback, Context } from "aws-lambda";
+import { DynamoDB } from "aws-sdk";
+import * as AWSXRay from 'aws-xray-sdk'
+
+// Verificara todos os serviços aws
+AWSXRay.captureAWS(require('aws-sdk'))
+
+const eventsDdb = String(process.env.EVENTS_DDB)
+const ddbClient = new DynamoDB.DocumentClient()
+
+export async function handler(event: ProductEvent, context: Context, callback: Callback): Promise<void> {
+  // TODO - to be removed
+  console.log(event)
+
+  console.log(`Lambda requestId: ${context.awsRequestId}`)
+
+  await createEvent(event)
+
+  callback(null, JSON.stringify({
+    productEventCreated: true,
+    message: "OK"
+  }))
+}
+
+function createEvent(event: ProductEvent) {
+  const timestamp = Date.now()
+  // 5 minutos para apagar o dado
+  const ttl = ~~(timestamp / 1000) + 5 * 60 // 5 minutos a frente : ~~ = arredonda o valor
+
+  return ddbClient.put({
+    TableName: eventsDdb,
+    Item: {
+      pk: `#product_${event.productCode}`,
+      sk: `${event.eventType}#${timestamp}`, // PRODUCT_CREATED#123456
+      email: event.email,
+      createdAt: timestamp,
+      requestId: event.requestId,
+      eventType: event.eventType,
+      info: {
+        productId: event.productId,
+        price: event.productPrice
+      },
+      ttl
+    }
+  }).promise()
+}
